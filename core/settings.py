a32c8d55c2a9b3a50d8efbe104bbf24de4b2cdd1
Javier Villarreal <villarrealjavier401@gmail.com>
2025-06-18 23:25:54 -0500

    feat: graphQL basic setup was added

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'universe'
+    'universe',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
+from core.scheme import schema  # Adjust the import based on your project structure
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),  # Adjust the schema import as needed
 ]

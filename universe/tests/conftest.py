1e5cb34aae87ae513b6d24a1c9dec396b239b9f8
Javier Villarreal <villarrealjavier401@gmail.com>
2025-06-20 13:58:14 -0500

    test: initial setup was done

---
+++ universe/tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from datetime import date
+from universe.models import Movie, Character, Planet
+
+@pytest.fixture
+def planet():
+    return Planet.objects.get_or_create(
+        name="Test planet"
+    )[0]
+
+@pytest.fixture
+def character():
+    return Character.objects.get_or_create(
+        name="Test subject"
+    )[0]
+
+@pytest.fixture
+def movie(planet, character):
+    movie = Movie.objects.get_or_create(
+        opening_crawl= "First test is being created",
+        title="Test movie",
+        director="Javier Villarreal",
+        producers="1,2",
+        created_date=date.today()
+    )[0]
+    movie.planets.set([planet])
+    movie.characters.set([character])
+
+    return movie
